3246e4e38842b41b4a47e86c36cb21bd90d638d9
Navyabhat03 <60884505+Navyabhat03@users.noreply.github.com>
2023-06-15 09:08:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,76 +1,62 @@
 from tqdm import tqdm
-import torch
-import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 train_losses = []
 test_losses = []
 train_acc = []
 test_acc = []
 
-def GetCorrectPredCount(pPrediction, pLabels):
-  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
 
-def train(model, device, train_loader, optimizer, criterion):
-  model.train()
-  pbar = tqdm(train_loader)
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+        # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
 
-  train_loss = 0
-  correct = 0
-  processed = 0
+        # Predict
+        y_pred = model(data)
 
-  for batch_idx, (data, target) in enumerate(pbar):
-    data, target = data.to(device), target.to(device)
-    optimizer.zero_grad()
+        # Calculate loss
+        loss = F.nll_loss(y_pred, target)
+        train_losses.append(loss)
 
-    # Predict
-    pred = model(data)
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
 
-    # Calculate loss
-    loss = criterion(pred, target)
-    train_loss+=loss.item()
+        # Update pbar-tqdm
 
-    # Backpropagation
-    loss.backward()
-    optimizer.step()
-    
-    correct += GetCorrectPredCount(pred, target)
-    processed += len(data)
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
 
-    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        train_acc.append(100*correct/processed)
 
-  train_acc.append(100*correct/processed)
-  train_losses.append(train_loss/len(train_loader))
-
-def test(model, device, test_loader, criterion):
+def test(model, device, test_loader):
     model.eval()
-
     test_loss = 0
     correct = 0
-
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
+        for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-
             output = model(data)
-            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
-
-            correct += GetCorrectPredCount(output, target)
-
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    test_acc.append(100. * correct / len(test_loader.dataset))
     test_losses.append(test_loss)
 
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-
-    fig, axs = plt.subplots(2,2,figsize=(15,10))
-    axs[0, 0].plot(train_losses)
-    axs[0, 0].set_title("Training Loss")
-    axs[1, 0].plot(train_acc)
-    axs[1, 0].set_title("Training Accuracy")
-    axs[0, 1].plot(test_losses)
-    axs[0, 1].set_title("Test Loss")
-    axs[1, 1].plot(test_acc)
-    axs[1, 1].set_title("Test Accuracy")
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
